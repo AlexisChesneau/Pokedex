a83cf624d5624532f7e1d3b00fc239398b50f834
Alexis Chesneau <alexischesneau64@gmail.com>
2023-12-30 03:05:57 +0100

    Co-authored-by: Antoine Meresse <AntoineMeresse@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -10,53 +10,75 @@ const typeContainerSpells = document.getElementById("typeContainerSpells");
 const API = `https://tyradex.vercel.app/api/v1/gen/1`;
 
 let indexPokemon = 0;
+let data;
+
+fetch(API)
+  .then((res) => res.json())
+  .then((apiData) => {
+    data = apiData;
+    afficherPokemon();
+  })
+  .catch((err) => console.log(err));
 
 function afficherPokemon() {
-  fetch(API)
-    .then((res) => res.json())
-    .then((data) => {
-      const typePokemon = data[indexPokemon].types;
-      const spellPokemon = data[indexPokemon].talents;
+  const typePokemon = data[indexPokemon].types;
+  const spellPokemon = data[indexPokemon].talents;
 
-      const resetElement = document.querySelectorAll(".imgType, .spellName");
-      resetElement.forEach((element) => {
-        element.remove();
-      });
+  const resetElement = document.querySelectorAll(".imgType, .spellName");
+  resetElement.forEach((element) => {
+    element.remove();
+  });
 
-      namePokemon.innerHTML = data[indexPokemon].name.fr;
-      imgPokemon.src = data[indexPokemon].sprites.regular;
+  namePokemon.innerHTML = data[indexPokemon].name.fr;
+  imgPokemon.src = data[indexPokemon].sprites.regular;
 
-      for (let i = 0; i < (typePokemon.length, spellPokemon.length); i++) {
-        const imgType = document.createElement("img");
-        const spellName = document.createElement("p");
+  for (let i = 0; i < (typePokemon.length, spellPokemon.length); i++) {
+    const imgType = document.createElement("img");
+    const spellName = document.createElement("p");
 
-        imgType.classList.add("imgType");
-        spellName.classList.add("spellName");
+    imgType.classList.add("imgType");
+    spellName.classList.add("spellName");
 
-        if (i < typePokemon.length) {
-          imgType.src = typePokemon[i].image;
-          typeContainerTypes.appendChild(imgType);
-        }
+    if (i < typePokemon.length) {
+      imgType.src = typePokemon[i].image;
+      typeContainerTypes.appendChild(imgType);
+    }
 
-        if (i < spellPokemon.length) {
-          spellName.innerHTML = spellPokemon[i].name;
-          typeContainerSpells.appendChild(spellName);
-        }
-      }
-      TypeBackground(typePokemon);
-    })
-    .catch((err) => console.log(err));
+    if (i < spellPokemon.length) {
+      spellName.innerHTML = spellPokemon[i].name;
+      typeContainerSpells.appendChild(spellName);
+    }
+  }
+  TypeBackground(typePokemon);
+}
+
+function decrementIndex() {
+  if (indexPokemon > 0) {
+    indexPokemon--;
+    nextButton.disabled = false;
+  } else {
+    previousButton.disabled = true;
+  }
+}
+
+function incrementIndex() {
+  if (indexPokemon < data.length - 1) {
+    indexPokemon++;
+    previousButton.disabled = false;
+  } else {
+    nextButton.disabled = true;
+  }
 }
 
 function PreviousPokemon() {
-  afficherPokemon(indexPokemon--);
+  decrementIndex();
+  afficherPokemon();
 }
 
 function NextPokemon() {
-  afficherPokemon(indexPokemon++);
+  incrementIndex();
+  afficherPokemon();
 }
 
 previousButton.addEventListener("click", PreviousPokemon);
 nextButton.addEventListener("click", NextPokemon);
-
-afficherPokemon();
